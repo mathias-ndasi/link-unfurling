52c7fed7c3225057c18bce5a74668c2c7e1eced8
Mathias Ndasi <42352069+mathias-ndasi@users.noreply.github.com>
2021-11-03 22:19:26 +0100

    update main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-// import { AllExceptionsFilter } from './exceptions/http-exception.filter';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
@@ -16,9 +15,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  // Register our global exception handler
-  // app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(process.env.PORT, () =>
     Logger.log(`Server listening on port:${process.env.PORT}`),
